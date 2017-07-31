b93d7f97dfdf6656093ad0920b1413ccfa15bbe2
doker42 <doker42@gmail.com>
2017-07-31 19:20:12 +0300

    5
    used FLOCK

--- visitors.php
+++ visitors.php
@@ -1,6 +1,11 @@
 <?php
 error_reporting(E_ALL);
 
+function get_content($file_name){    //функция для получения значения счетчика
+    $count = (int)file_get_contents($file_name, true);
+    return $count;
+}
+
 $db_name = 'database.txt'; //имя файла с данными счетчика
 
 if(!empty($_POST['name'])) {
@@ -9,11 +14,23 @@ if(!empty($_POST['name'])) {
 
 if(!isset($_COOKIE['name'])) {      //если Нет Куки
     $message = '<p style="color: red; font-weight: bold">The SITE doesn\'t have  $_COOKIE</p>'.'<br>';
-    setcookie('name', $name, time()+3600);   // устанавливаем Куки
+    setcookie('name', $name, time()+5);   // устанавливаем Куки
 
     if(is_readable($db_name)){    //если файл существует
-        file_put_contents($db_name, (((int)file_get_contents($db_name, true))+1));         // записываем новое значение счетчика
-        $my_count = (int)file_get_contents($db_name, true);
+
+        //скрипт без "Лока"
+//        file_put_contents($db_name, (((int)file_get_contents($db_name, true))+1));         // записываем новое значение счетчика
+//        $my_count = (int)file_get_contents($db_name, true);
+
+        //скрипт с "Локом"
+        if (flock(fopen($db_name, 'r+'), LOCK_EX)) {
+            $my_count = get_content($db_name)+1;
+            //$my_count = $my_count + 1;
+            ftruncate(fopen($db_name, 'r+'), 0);
+            fwrite(fopen($db_name, 'w+'), $my_count);
+            flock(fopen($db_name, 'r+'), LOCK_UN);
+        } else { $message_1 = "No block!";}
+        fclose(fopen($db_name, 'r+'));
 
     } else {
         $my_count = 0;              //если файла нет, создаем значение счетчика
@@ -54,6 +71,7 @@ if(!isset($_COOKIE['name'])) {      //если Нет Куки
 <!--<a href="pattern.php" style="color: red; font-size: 20px; font-weight: bold">Go to the PATTERN</a>-->
 <br>
 <br>
+<p style="color: forestgreen"> <p style="color: #800080"> <?php if(isset($message_1)) echo $message_1; ?> </p></p>
 <br>
 <br>
 
